06e5cf854a08b0bca0f0e0f566c8d88cb4a09762
Jonas Singbo <jnax09@gmail.com>
2019-01-06 08:34:23 +0200

    Initial commit

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-  return (
-    <footer className="bg-dark text-white p-4 text-center">
-        Copyright &copy; { new Date().getFullYear() } BB Tech
-    </footer>
-  )
-}
